437e36b5831cd3fbd93dd6d6533815f3e45384ca
will <maoguangxian@famulei.com>
2018-04-07 19:37:46 +0800

    binary search

--- main.go
+++ main.go
@@ -25,5 +25,6 @@ func getRandomIntSlice(min, max, size int) (randomIntSlice []int) {
 
 func main() {
 	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(search.SequenceSearch(data, 5))
+	fmt.Println(search.BinarySearchV1(data, 5, 0, 9))
+	fmt.Println(search.BinarySearchV2(data, 5))
 }
--- search/search.go
+++ search/search.go
@@ -12,3 +12,41 @@ func SequenceSearch(data []int, value int) (index int, founded bool) {
 
 	return
 }
+
+// BinarySearchV1 二分查找
+func BinarySearchV1(data []int, value int, low, high int) (index int, founded bool) {
+	mid := low + (high-low)/2
+	if data[mid] == value {
+		index = mid
+		founded = true
+		return
+	}
+
+	if data[mid] > value {
+		index, founded = BinarySearchV1(data, value, low, mid-1)
+	} else {
+		index, founded = BinarySearchV1(data, value, mid+1, high)
+	}
+
+	return
+}
+
+// BinarySearchV2 二分查找
+func BinarySearchV2(data []int, value int) (index int, founded bool) {
+	low, high := 0, len(data)-1
+	for low <= high {
+		mid := (high - low) / 2
+		if data[mid] == value {
+			index = mid
+			founded = true
+			return
+		}
+
+		if data[mid] > value {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return
+}
