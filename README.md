2b41524ac6859d11137081f50117d58b396bb6fb
will <maoguangxian@famulei.com>
2018-04-07 21:54:31 +0800

    fibonacci search

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ go常用算法
 ### 参考文档
 - [秒懂排序算法](https://cloud.tencent.com/developer/article/1081747)
 - [七大查找算法](http://www.cnblogs.com/maybe2030/p/4715035.html)
+- [查找算法总结](https://www.zybuluo.com/guoxs/note/369750)
--- search/search.go
+++ search/search.go
@@ -71,3 +71,51 @@ func InsertionSearch(data []int, value int) (index int, founded bool) {
 	}
 	return
 }
+
+func fibonacci(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	if n == 1 || n == 2 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+// FibonacciSearch 斐波那契查找
+func FibonacciSearch(data []int, value int) (index int, founded bool) {
+	low, high, k := 0, len(data)-1, 0
+
+	for len(data) > fibonacci(k)-1 {
+		k++
+	}
+	// fmt.Println("K -----> ", k)
+	tmp := make([]int, fibonacci(k)-1)
+	copy(tmp, data)
+	for i := len(data); i < fibonacci(k)-1; i++ {
+		tmp[i] = data[len(data)-1]
+	}
+
+	// fmt.Println(tmp)
+	for low <= high {
+		mid := low + fibonacci(k-1) - 1
+		// fmt.Println("mid ----->", mid)
+		if value < tmp[mid] {
+			high = mid - 1
+			k--
+		} else if value > tmp[mid] {
+			low = mid + 1
+			k -= 2
+		} else {
+			if mid < len(data) {
+				index = mid
+				founded = true
+			} else {
+				index = len(data) - 1
+				founded = true
+			}
+			return
+		}
+	}
+	return
+}
